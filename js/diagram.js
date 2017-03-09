5f1744b11ded1b03bf7213da61fa4a0bc63e46fb
Thomas Sibley <trsibley@uw.edu>
2017-03-09 11:17:14 -0800

    Rework the diagram component to re-use the same Vega view
    Instead of re-parsing the spec and re-creating the view every time the
    data changed.  This is more parsimonious, but the primary upshot is that
    the view isn't temporarily removed from the DOM before being replaced,
    and thus doesn't cause a flash of content or force the viewport to
    scroll to the top.  These side-effects weren't nearly as noticeable
    before the page required scrolling.
    
    Another upshot is that a bunch of other callbacks can directly close
    over the view object and know that it must exist rather than looking for
    it on the element and checking if it (and the element itself!) still
    exist.

--- js/diagram.js
+++ js/diagram.js
@@ -18,86 +18,106 @@
       replace: false,
       scope: {
         data: '<',
-        toUrl: '=',
-        rendered: '='
+        toUrl: '=?',
+        rendered: '=?'
       },
       link: function (elementScope, element, attrs) {
-        // When our input data changes, re-render the Vega spec.
-        elementScope.$watch('data', function(newValue, oldValue) {
-          if (!newValue) {
-            elementScope.rendered = false;
-            element[0].childNodes.forEach(function(child){
-              child.remove()
+        $log.debug("Parsing Vega spec");
+
+        // Parse the Vega spec, create a Vega view attached to our element, and
+        // update it with any starting data we have at this point.
+        //
+        // Everything hinges on a successful parsing of the spec.  If that
+        // fails, then nothing else is done.
+        //
+        vg.parse.spec(MethylationDiagramSpec, function(error, chart) {
+          elementScope.$apply(function(scope) {
+            if (error) {
+              $log.error("Error parsing Vega spec: " + error);
+              scope.rendered = false;
+              return;
+            }
+
+            // Create Vega view for this visualization spec.  The view is
+            // stashed on the DOM element for easier debugging.
+            var view = element[0].view = chart({
+              el: element[0],
+              renderer: "svg"
             });
-            return;
-          }
 
-          vg.parse.spec(MethylationDiagramSpec, function(error, chart) {
-            elementScope.$apply(function(scope) {
-              if (error) {
-                $log.error("Error parsing Vega spec: " + error);
-                scope.rendered = false;
-                return;
-              }
+            function replaceViewData(data) {
+              $log.debug("Replacing Vega view data for methylation diagram");
 
-              var view = element[0].view = chart({
-                el: element[0],
-                renderer: "svg"
-              });
-
-              view.data("alignment")
-                .insert(newValue);
-
+              // This ensures that the visualization and view's width/height
+              // calculations are done from scratch on the new data by forcing
+              // a width update to 0 and back.  Kinda a hack.
+              view.width(0).update();
               view.width(element[0].clientWidth);
 
+              view.data("alignment")
+                .remove(function(){ return true })
+                .insert(data || []);
+
               view.update();
-              scope.rendered = true;
 
-              // Update our directive's scope with a new toUrl function closing
-              // over the new Vega view object.
-              //
-              // The XML header is required for viewing the SVG outside of the
-              // browser.  It is from:
-              //
-              //    https://github.com/vega/vega/blob/v2.x/bin/vg2svg
-              //
-              scope.toUrl = function() {
-                var svgHeader =
-                  '<?xml version="1.0" encoding="utf-8"?>\n' +
-                  '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ' +
-                  '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+              scope.rendered = !!data;
+            }
 
-                var svg  = view.renderer().svg();
-                var defs = document.querySelector("svg#gradients defs").outerHTML;
+            // If we have starting data, render it now.
+            if (scope.data)
+              replaceViewData(scope.data);
 
-                // Embed external definitions for gradient fills at the end of
-                // the document.  Yes, this is doing string manip on XML.
-                svg = svg.replace(/(?=<\/svg>)/i, defs);
-
-                var blob = new Blob([svgHeader + svg], {type: 'image/svg+xml'});
-                return window.URL.createObjectURL(blob);
-              };
+            // When our input data changes, update the Vega view.
+            scope.$watch('data', function(newValue, oldValue) {
+              if (newValue !== oldValue)
+                replaceViewData(newValue);
             });
+
+            // Update the Vega view when our element's width changes
+            scope.$watch(
+              function(){ return element[0].clientWidth },
+              function(newValue, oldValue) {
+                if (newValue)
+                  view.width(newValue).update();
+              }
+            );
+
+            // The resize event normally wouldn't trigger an angular scope digest,
+            // meaning our watch above wouldn't fire.  We manually ask the scope to
+            // update at most every 500ms during a stream of resize events.
+            window.addEventListener(
+              'resize',
+              debounce(500, scope.$apply.bind(scope)),
+              false
+            );
+
+            // Define a toUrl function exported into our scope which returns a URL
+            // pointing to the SVG contents of the Vega view.
+            //
+            // The XML header is required for viewing the SVG outside of the
+            // browser.  It is from:
+            //
+            //    https://github.com/vega/vega/blob/v2.x/bin/vg2svg
+            //
+            scope.toUrl = function() {
+              var svgHeader =
+                '<?xml version="1.0" encoding="utf-8"?>\n' +
+                '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ' +
+                '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+
+              var svg  = view.renderer().svg();
+              var defs = document.querySelector("svg#gradients defs").outerHTML;
+
+              // Embed external definitions for gradient fills at the end of
+              // the document.  Yes, this is doing string manip on XML.
+              svg = svg.replace(/(?=<\/svg>)/i, defs);
+
+              var blob = new Blob([svgHeader + svg], {type: 'image/svg+xml'});
+              return window.URL.createObjectURL(blob);
+            };
+
           });
         });
-
-        // Update the Vega view when our element's width changes
-        elementScope.$watch(
-          function(){ return element[0].clientWidth },
-          function(newValue, oldValue) {
-            if (element[0] && element[0].view && newValue)
-              element[0].view.width(newValue).update();
-          }
-        );
-
-        // The resize event normally wouldn't trigger an angular scope digest,
-        // meaning our watch above wouldn't fire.  We manually ask the scope to
-        // update at most every 500ms during a stream of resize events.
-        window.addEventListener(
-          'resize',
-          debounce(500, elementScope.$apply.bind(elementScope)),
-          false
-        );
       }
     };
   }
